1c1403a50c84cce67dd01c214d4cdf22de8bc432
mootezrhouma <mootez94@gmail.com>
2023-03-23 21:48:19 +0100

    react1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Formulaire from './components/Formulaire'; 
+
+function App() {
+  return (
+    <div className="App">
+    <Formulaire/>
+    </div>
+  );
+}
+
+export default App;
